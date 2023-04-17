b0c1db0ccaf5707d77f92f89d5b1e0b3be6a237e
jhordy <jhooordy@gmail.com>
2023-04-17 13:43:59 -0300

    fix: CR-01 Unprotected Funds Withdrawal Critical

--- contracts/SnackclubTournamentsPayout.sol
+++ contracts/SnackclubTournamentsPayout.sol
@@ -1,47 +1,62 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.11;
 
-import './RewardControl.sol';
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import "./RewardControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SnackclubTournamentsPayout is RewardControl {
-  event RewardClaimed(address indexed to, uint256 amount, address tokenContract);
+    event RewardClaimed(
+        address indexed to,
+        uint256 amount,
+        address tokenContract
+    );
 
-  function claimReward(string memory secret) public {
-    bytes32 hashedWallet = hashAddressAndString(msg.sender, secret);
+    function claimReward(string memory secret) public {
+        bytes32 hashedWallet = hashAddressAndString(msg.sender, secret);
 
-    Reward memory r = reward[hashedWallet];
+        Reward memory r = reward[hashedWallet];
 
-    uint256 addThirdDays = r.claimableAt + 30 days;
-    bool compareDates = block.timestamp <= addThirdDays;
+        uint256 addThirdDays = r.claimableAt + 30 days;
+        bool compareDates = block.timestamp <= addThirdDays;
 
-    if (!compareDates) {
-      delete reward[hashedWallet];
-      require(compareDates, 'This reward is so old to get claimed');
+        if (!compareDates) {
+            delete reward[hashedWallet];
+            require(compareDates, "This reward is so old to get claimed");
+        }
+
+        IERC20 token = IERC20(r.tokenContract);
+        require(
+            getTokenBalance(r.tokenContract) >= r.amount,
+            "insufficient token balance"
+        );
+        require(token.transfer(msg.sender, r.amount), "transfer failed");
+
+        delete reward[hashedWallet];
+        emit RewardClaimed(msg.sender, r.amount, r.tokenContract);
     }
 
-    IERC20 token = IERC20(r.tokenContract);
-    require(getTokenBalance(r.tokenContract) >= r.amount, 'insufficient token balance');
-    require(token.transfer(msg.sender, r.amount), 'transfer failed');
+    function getTokenBalance(
+        address tokenContract
+    ) public view returns (uint256) {
+        IERC20 token = IERC20(tokenContract);
+        return token.balanceOf(address(this));
+    }
 
-    delete reward[hashedWallet];
-    emit RewardClaimed(msg.sender, r.amount, r.tokenContract);
-  }
+    function withdraw(uint256 amount, address tokenContract) public onlyAdmin {
+        require(getTokenBalance(tokenContract) >= amount, "Insufficient funds");
 
-  function getTokenBalance(address tokenContract) public view returns (uint256) {
-    IERC20 token = IERC20(tokenContract);
-    return token.balanceOf(address(this));
-  }
+        IERC20 token = IERC20(tokenContract);
+        require(token.transfer(msg.sender, amount), "transfer failed");
+    }
 
-  function withdraw(uint256 amount, address tokenContract) public {
-    require(getTokenBalance(tokenContract) >= amount, 'Insufficient funds');
-
-    IERC20 token = IERC20(tokenContract);
-    require(token.transfer(msg.sender, amount), 'transfer failed');
-  }
-
-  function depositToken(uint256 amount, address tokenContract) public onlyAdminOrOperator {
-    IERC20 token = IERC20(tokenContract);
-    require(token.transferFrom(msg.sender, address(this), amount), 'Transfer failed');
-  }
+    function depositToken(
+        uint256 amount,
+        address tokenContract
+    ) public onlyAdminOrOperator {
+        IERC20 token = IERC20(tokenContract);
+        require(
+            token.transferFrom(msg.sender, address(this), amount),
+            "Transfer failed"
+        );
+    }
 }
